91563013e2ea9fc752dc3fbdbd02cf390ff35527
Andrew Enfield <aenfield@gmail.com>
2018-04-11 16:26:55 -0700

    Finished first cut at both problems - straightforward, so just this one checkin.

---
+++ homework3.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import sqlite3
+
+def create_dataframe(db_filename):
+    """
+    Creates a dataframe by concatenating the tables in the database specified
+    by db_filename, following the specific instructions in the homework.
+    """
+    conn = sqlite3.connect(db_filename)	
+    
+    sql = """
+    SELECT video_id, category_id, "ca" AS language FROM CAvideos
+    UNION
+    SELECT video_id, category_id, "de" AS language FROM DEvideos
+    UNION
+    SELECT video_id, category_id, "fr" AS language FROM FRvideos
+    UNION
+    SELECT video_id, category_id, "gb" AS language FROM GBvideos
+    UNION
+    SELECT video_id, category_id, "us" AS language FROM USvideos
+    ;
+    """
+
+    # sqlite3.connect doesn't raise an error if the path to the database
+    # is invalid - it only fails when you try to use the connection
+    # to actually execute a query; we'll catch the failure here and re-raise
+    # as the homework-specified ValueError
+    try:
+        return pd.read_sql_query(sql, conn)
+    except pd.io.sql.DatabaseError:
+        raise ValueError("Failed executing query with specified database path.")
+
+    return df
+
+if __name__ == '__main__':
+    df = create_dataframe('../HW1-aenfield/class.db')
+    print(df.shape)
+    print(test_create_dataframe(df, verbose=True))
---
+++ test_homework3.py
@@ -0,0 +1,32 @@
+import unittest
+from homework3 import create_dataframe  # not good generally but ok for one method here
+
+class CreateDataFrameMethods(unittest.TestCase):
+
+    def setUp(self):
+        # create df with correct path
+        self.df = create_dataframe('../HW1-aenfield/class.db')
+        
+    def test_has_only_correct_column_names(self):
+        # contains only the columns video_id, category_id, language
+        expected_columns = ['video_id','category_id','language']
+        for colname in expected_columns:
+            self.assertIn(colname, self.df.columns)
+        # also need to confirm there's only three columns, for the 'only' part
+        self.assertEqual(3, len(self.df.columns))
+       
+    def test_df_has_at_least_10_rows(self):
+        self.assertTrue(len(self.df) >= 10)
+
+    def test_df_vals_are_not_a_key(self):
+        # I'm going to test that the specified values are _not_ a key, since
+        # I know that they're not a key, and I want these tests to pass
+        self.assertNotEqual(len(self.df), len(self.df.groupby(['video_id','language']).size()))
+
+    def test_invalid_path_raises(self):
+        with self.assertRaises(ValueError):
+            create_dataframe('abadpath')
+            
+
+if __name__ == '__main__':
+    unittest.main()
